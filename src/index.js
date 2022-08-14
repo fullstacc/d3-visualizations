ae15828b5d999985b967cf8a55fe6e318680a600
Jason Summercamp <bbetancova@gmail.com>
2022-08-14 07:17:18 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>D3 Visualizations</h1>
+</div>
+`;
+
+document.getElementById("viz-1").innerHTML = `
+<h1>Bar Chart</h1>
+</div>
+`;
